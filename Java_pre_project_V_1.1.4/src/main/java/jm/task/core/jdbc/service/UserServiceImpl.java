0d66fbdef329276d3012c2d89f4e7e9ab2569efc
RomanB <rad18koz@gmail.com>
2024-05-13 20:32:45 +0300

    UserDaoJDBCUpdate

--- Java_pre_project_V_1.1.4/src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ Java_pre_project_V_1.1.4/src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,5 +1,7 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.util.Util;
 import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.model.User;
@@ -16,109 +18,36 @@ public class UserServiceImpl implements UserService {
 
     public void createUsersTable() {
 
-       Util util = Util.getInstance();
-       Connection conn = util.getConn();
-
-       try {
-
-           Statement stmt = conn.createStatement();
-           stmt.executeUpdate("CREATE TABLE IF NOT EXISTS `user`.`user` (`id` INT NOT NULL AUTO_INCREMENT,`name` VARCHAR(45) NOT NULL,`lastName` VARCHAR(45) NOT NULL,`age` INT NOT NULL,PRIMARY KEY (`id`),UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);");
-
-       } catch (SQLException e){
-
-           e.printStackTrace();
-
-       }
-
+        UserDaoJDBCImpl  userDaoJDBC= new UserDaoJDBCImpl();
+        userDaoJDBC.createUsersTable();
     }
 
     public void dropUsersTable() {
 
-        Util util = Util.getInstance();
-        Connection conn = util.getConn();
-
-        try {
-
-            Statement stmt = conn.createStatement();
-            stmt.executeUpdate("DROP TABLE IF EXISTS`user`.`user`;");
-
-        } catch (SQLException e){
-
-            e.printStackTrace();
-
-        }
+        UserDaoJDBCImpl  userDaoJDBC= new UserDaoJDBCImpl();
+        userDaoJDBC.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        Util util = Util.getInstance();
-        Connection conn = util.getConn();
 
-        try {
-
-            Statement stmt = conn.createStatement();
-            stmt.executeUpdate("INSERT INTO `user`.`user` (name, lastname, age) VALUES ('" + name + "', '" + lastName+ "', " + age +")");
-            System.out.println("User с именем — " + name + " добавлен в базу данных");
-
-        } catch (SQLException e){
-
-            e.printStackTrace();
-
-        }
+        UserDaoJDBCImpl  userDaoJDBC= new UserDaoJDBCImpl();
+        userDaoJDBC.saveUser(name, lastName, age);
     }
 
-    public void removeUserById(long id)
-    {
-        Util util = Util.getInstance();
-        Connection conn = util.getConn();
+    public void removeUserById(long id) {
 
-        try {
-
-            Statement stmt = conn.createStatement();
-            stmt.executeUpdate("DELETE FROM `user`.`user` WHERE `id` = " + id);
-
-        } catch (SQLException e){
-
-            e.printStackTrace();
-
-        }
+        UserDaoJDBCImpl  userDaoJDBC= new UserDaoJDBCImpl();
+        userDaoJDBC.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
 
-        Util util = Util.getInstance();
-        Connection conn = util.getConn();
-        List<User> users = new ArrayList<User>();
-
-        try {
-
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM `user`.`user`;");
-
-            while (rs.next()) {
-
-                User user = new User();
-                user.setId(rs.getLong("id"));
-                user.setName(rs.getString("name"));
-                user.setLastName(rs.getString("lastName"));
-                user.setAge(rs.getByte("age"));
-                users.add(user);
-
-            }
-
-        } catch (SQLException e){
-
-            e.printStackTrace();
-
-        }
-
-        return users;
+        UserDaoJDBCImpl  userDaoJDBC= new UserDaoJDBCImpl();
+        return userDaoJDBC.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        int temp = 1;
-        while (temp < 10000){
-            removeUserById(temp);
-            temp++;
-        }
+        UserDaoJDBCImpl  userDaoJDBC= new UserDaoJDBCImpl();
+        userDaoJDBC.cleanUsersTable();
     }
 }
